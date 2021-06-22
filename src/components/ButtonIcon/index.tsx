f47f1c20c126c6c36cec1113c101840f5ac50abf
Sergio Victor <sergiovmml@gmail.com>
2021-06-22 00:07:20 -0300

    1

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -11,14 +11,21 @@ import {
   import { styles } from './styles';
 
 
-  export function ButtonIcon(){
+  type Props = TouchableOpacityProps & {
+    title: string;
+  }
+
+  export function ButtonIcon({ title, ...rest } : Props){
       return (
-          <TouchableOpacity style={styles.container}>
+          <TouchableOpacity 
+          style={styles.container}
+          {...rest }
+          >
           <View style={styles.iconWrapper}>
             <Image source={DiscordImg} style={styles.icon} />
           </View>
           <Text style={styles.title} >
-            Entrar com Discord
+            {title}
           </Text>
           </TouchableOpacity>
       )
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -37,7 +37,10 @@ export function SignIn() {
                     Crie grupos para jogar seus games {'\n'}
                     favoritos com seus amigos
                 </Text>
-                <ButtonIcon />
+                <ButtonIcon 
+                title="Entrar Discord" 
+                activeOpacity={0.7}
+                />
             </View>
         </View>
     )
