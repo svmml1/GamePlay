11ed386ce0c70357fe682c8f90d149410260ea3e
Sergio Victor <sergiovmml@gmail.com>
2021-06-25 23:02:22 -0300

    1

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -11,15 +11,17 @@ type Props = RectButtonProps & {
   title: string;
   icon: React.FC<SvgProps>;
   checked?: boolean;
+  hasCheckBox?: boolean;
 }
 
 export function Category({
   title,
   icon: Icon,
   checked = false,
+  hasCheckBox = true,
   ...rest
 }: Props){
-  const { secondary50, secondary70 } = theme.colors;
+  const { secondary40, secondary50, secondary70, secondary85 } = theme.colors;
 
   return(
     <RectButton {...rest}>
@@ -27,10 +29,16 @@ export function Category({
       style={styles.container}
       colors={[secondary50, secondary70]}
       >
-        <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
-          <View style={
-            checked ? styles.checked : styles.check
-          }/>
+        <LinearGradient style={[styles.content, { opacity: checked ? 1 : 0.5 }]}
+        colors={[checked ? secondary85 : secondary50, secondary40 ]}
+        >
+         {
+            hasCheckBox &&
+            <View style={
+              checked ? styles.checked : styles.check
+            }/>
+          }
+          
 
           <Icon 
               width={48} 
@@ -40,7 +48,7 @@ export function Category({
           <Text style={styles.title}>
             { title }
           </Text>
-        </View>
+        </LinearGradient>
       </LinearGradient>
     </RectButton>
   );
