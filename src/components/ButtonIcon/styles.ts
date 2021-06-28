e07c1e99798090544f8d541d5bc3eb5d0ae5c2d9
Sergio Victor <sergiovmml@gmail.com>
2021-06-27 21:41:34 -0300

    Update styles.ts

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -4,7 +4,7 @@ import { theme } from '../../../global/styles/theme';
 
 export const styles = StyleSheet.create({
     container: {
-        width: '100%',
+        width: 296,
         height: 56,
         backgroundColor: theme.colors.primary,
         borderRadius:  8,
